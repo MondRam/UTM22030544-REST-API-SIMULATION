5104d3011a5763dfacb6c344b79fd504f08be3f2
MondRam <utm22030544@utma.edu.mx>
2024-03-03 18:17:41 -0600

    feat: the function dequeue was added

--- queue.js
+++ queue.js
@@ -25,5 +25,21 @@ class Queue {
             return "The element was added"
         }
     };
+    
+    dequeue(){
+        if(!this.first) return "there is no stack";
+        
+        if(this.first) {
+            if (this.first === this.last){
+                this.first = null;
+                this.last = null;
+                return "The element was the first and the last, it was removed";
+            }else{
+                this.first = this.first.next;
+                return "The first element was removed";
+            }
+            
+        }
+    };
 
 };
